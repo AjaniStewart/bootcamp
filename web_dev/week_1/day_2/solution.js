f2c35f36a9a487daf9b6d37ec835a98e90ca9c75
Ajani Stewart <theamjaha@gmail.com>
2019-07-17 13:43:10 -0400

    first parts of solution for assignment 2

---
+++ web_dev/week_1/day_2/solution.js
@@ -0,0 +1,42 @@
+Array.prototype.myForEach = function (callback, thisArg) {
+  if (thisArg === undefined) {
+    thisArg = this;
+  }
+  for (let i = 0; i < thisArg.length; i++) {
+    callback(thisArg[i], i, thisArg);
+  }
+}
+
+function myForEach(callback, array) {
+  for (let i = 0; i < array.length; i++) {
+    callback(array[i], i);
+  }
+}
+
+Array.prototype.myMap = function (callback, thisArg) {
+  if (thisArg === undefined) { thisArg = this; }
+  let newArray = [];
+  for (let i = 0; i < thisArg.length; i++) {
+    newArray.push(callback(thisArg[i], i, thisArg));
+  }
+  return newArray;
+}
+
+function myMap(callback, array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(callback(array[i], i));
+  }
+  return newArray;
+}
+Array.prototype.myFilter = function (callback, thisArg) {
+  if (thisArg === undefined) { thisArg = this; }
+
+  let newArray = [];
+  for (let i = 0; i < thisArg; i++) {
+    if (callback(thisArg[i], i, thisArg)) {
+      newArray.push(thisArg[i]);
+    }
+  }
+  return newArray;
+}
