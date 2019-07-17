1a28adf80fa5ea3905387128c6eeb7ca9465fde1
Ajani Stewart <theamjaha@gmail.com>
2019-07-17 13:58:34 -0400

    adds mySome

--- web_dev/week_1/day_2/solution.js
+++ web_dev/week_1/day_2/solution.js
@@ -50,3 +50,29 @@ function myFilter(callback, array) {
   }
   return newArray;
 }
+
+Array.prototype.mySome = function (callback, thisArg) {
+  if (thisArg === undefined) { thisArg = this; }
+
+  if (thisArg.length === 0) { return false; }
+
+  for (let i = 0; i < thisArg.length; i++) {
+    if (callback(thisArg[i], i, thisArg)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function mySome(callback, array) {
+  if (array.length === 0) { return false; }
+
+  for (let i = 0; i < array.length; i++) {
+    if (callback(thisArg[i], i, thisArg)) {
+      return true;
+    }
+  }
+
+  return false;
+}
